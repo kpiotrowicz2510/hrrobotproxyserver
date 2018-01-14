8b7fe93309f86a8dc852dfa05a758a3dd1c84694
Krzysztof Piotrowicz <krzychu2510@gmail.com>
2018-01-14 03:36:17 +0100

    cors

--- app.js
+++ app.js
@@ -55,13 +55,11 @@ var server = restify.createServer();
 server.get('/sendmessage/:message', respondMessage);
 server.get('/getmessages/:convId', respondGetMessage);
 server.head('/hello/:name', respond);
-server.use(
-    function crossOrigin(req,res,next){
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "X-Requested-With");
-      return next();
-    }
-  );
+server.use(restify.CORS({
+    origins: ['*'],
+    credentials: false, // defaults to false
+    headers: ['']  // sets expose-headers
+  }));
 server.listen(process.env.PORT || 80, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
